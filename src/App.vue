50d4a26a30331b772ea409b47b18a1f3d943dd80
tv_Cvt <tv@lovec.at>
2021-07-05 16:03:35 +0800

    feat: remove file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- 根路由 -->
+    <router-view />
   </div>
 </template>
 
@@ -17,13 +17,5 @@ import HelloWorld from './components/HelloWorld.vue'
 export default class App extends Vue {}
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
 </style>
